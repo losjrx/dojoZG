d247ff9c234514c10b924dd0938a96b371b9fa61
Lauis <losjrx@gmail.com>
2022-09-15 23:07:15 -0300

    "Organizando design"

--- portaoZG/src/com/portao/Controle.groovy
+++ portaoZG/src/com/portao/Controle.groovy
@@ -15,68 +15,24 @@ class Controle {
             char comando = input.charAt(i)
 
             // Verifica estado do portao, se aberto (5) true, se fechado (0) false
-            portao.aberto = (portao.status == 5)?true:portao.aberto
-            portao.aberto = (portao.status == 0)?false:portao.aberto
+            portao.check()
 
             // Portão fechado e não pausado
             if(!portao.aberto && !portao.pause){
-
-                if ((comando == '.' || comando == 'O') && !portao.abrindo) {
-                    saida.add(portao.status)
-
-                } else if((comando == '.' || comando == 'O')  && portao.abrindo){
-                    saida.add(++portao.status)
-
-                } else if(comando == 'P' && portao.abrindo){
-                    saida.add(portao.status)
-                    portao.pause = true
-
-                } else if(comando == 'P'){
-                    saida.add(++portao.status)
-                    portao.abrindo = true
-                }
+                portao.abrindoPortao(comando, saida)
 
             // Portão aberto e não pausado
             } else if(portao.aberto && !portao.pause){
+                portao.fechandoPortao(comando, saida)
 
-                if ((comando == '.' || comando == 'O') && !portao.fechando) {
-                    saida.add(portao.status)
-
-                } else if(comando == '.' && portao.fechando){
-                    saida.add(--portao.status)
-
-                } else if(comando == 'O' && portao.fechando){
-                    saida.add(++portao.status)
-                    portao.fechando = false
-                    portao.abrindo = true
-                    portao.aberto = false
-
-                } else if(comando == 'P' && portao.fechando){
-                    saida.add(portao.status)
-                    portao.pause = true
-
-                } else if(comando == 'P'){
-                    saida.add(--portao.status)
-                    portao.fechando = true
-                    portao.abrindo = false
-                }
-
-            } else if(portao.pause && comando != 'P'){
-                saida.add(portao.status)
-            } else if(portao.pause && portao.abrindo && comando == 'P'){
-                saida.add(++portao.status)
-                portao.pause = false
-            } else if(portao.pause && portao.fechando && comando == 'P') {
-                saida.add(--portao.status)
-                portao.pause = false
+            } else if(portao.pause){
+                portao.portaoPausado(comando, saida)
             }
+
         }
 
-        String output = "";
-        for(int b; b < saida.size(); b++){
-            output += saida.get(b)
-        }
-        return output
+        return portao.output(saida)
+
     }
 
 }
--- portaoZG/src/com/portao/Portao.groovy
+++ portaoZG/src/com/portao/Portao.groovy
@@ -7,4 +7,78 @@ class Portao {
     boolean abrindo = false
     boolean fechando = false
 
+    // Verifica estado do portão
+    void check(){
+        this.aberto = (this.status == 5)?true:this.aberto
+        this.aberto = (this.status == 0)?false:this.aberto
+    }
+
+    // As funções verificam as entradas e colocam na lista as saídas esperadas
+
+    void abrindoPortao(char comando, def saida){
+
+        if ((comando == '.' || comando == 'O') && !this.abrindo) {
+            saida.add(this.status)
+
+        } else if((comando == '.' || comando == 'O')  && this.abrindo){
+            saida.add(++this.status)
+
+        } else if(comando == 'P' && this.abrindo){
+            saida.add(this.status)
+            this.pause = true
+
+        } else if(comando == 'P'){
+            saida.add(++this.status)
+            this.abrindo = true
+        }
+
+    }
+
+    void fechandoPortao(char comando, def saida){
+
+        if ((comando == '.' || comando == 'O') && !this.fechando) {
+            saida.add(this.status)
+
+        } else if(comando == '.' && this.fechando){
+            saida.add(--this.status)
+
+        } else if(comando == 'O' && this.fechando){
+            saida.add(++this.status)
+            this.fechando = false
+            this.abrindo = true
+            this.aberto = false
+
+        } else if(comando == 'P' && this.fechando){
+            saida.add(this.status)
+            this.pause = true
+
+        } else if(comando == 'P'){
+            saida.add(--this.status)
+            this.fechando = true
+            this.abrindo = false
+        }
+
+    }
+
+    void portaoPausado(char comando, def saida){
+        if(comando != 'P'){
+            saida.add(this.status)
+        } else if(this.abrindo && comando == 'P'){
+            saida.add(++this.status)
+            this.pause = false
+        } else if(this.fechando && comando == 'P') {
+            saida.add(--this.status)
+            this.pause = false
+        }
+    }
+
+    // Recebe a lista das saídas e retorna String
+    String output(ArrayList saida){
+        String output = "";
+        for(int b; b < saida.size(); b++){
+            output += saida.get(b)
+        }
+        return output
+    }
+
 }
